2dee2b443b31dc2214e839d7d0545ced15155f18
RD <rajudhumne@gmail.com>
2018-01-18 15:55:20 +0530

    added progress UILabel

--- SpecificGoals-app/Controller/GoalsVC.swift
+++ SpecificGoals-app/Controller/GoalsVC.swift
@@ -30,6 +30,11 @@ class GoalsVC: UIViewController {
     }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(animated)
+       fetchCoreDataObject()
+        tableView.reloadData()
+    }
+
+    func fetchCoreDataObject () {
         self.fetch { (complete) in
             if complete {
                 if goals.count >= 1 {
@@ -39,10 +44,9 @@ class GoalsVC: UIViewController {
                 }
             }
         }
-        tableView.reloadData()
+    
     }
-
-   
+    
     @IBAction func addGoalBtnPressed(_ sender: Any) {
         guard let createGoalVC = storyboard?.instantiateViewController(withIdentifier: "CreateGoalVC") else {return}
         presentDetails(createGoalVC)
@@ -66,6 +70,38 @@ extension GoalsVC:UITableViewDelegate,UITableViewDataSource {
        cell.configureCell(goal: goal)
         return cell
     }
+    
+    
+    func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        return true
+    }
+    
+    func tableView(_ tableView: UITableView, editingStyleForRowAt indexPath: IndexPath) -> UITableViewCellEditingStyle {
+        return .none
+    }
+    
+    func tableView(_ tableView: UITableView, editActionsForRowAt indexPath: IndexPath) -> [UITableViewRowAction]? {
+        let deleteAction = UITableViewRowAction(style: .destructive, title: "DELETE") { (rowAction, indexPath) in
+            self.removeGoal(atIndexPath: indexPath)
+            self.fetchCoreDataObject()
+            tableView.deleteRows(at: [indexPath], with: .automatic)
+            
+           
+        }
+        
+        let addAction = UITableViewRowAction(style: .normal, title: "ADD 1") { (rowAction, indexpath) in
+            self.setProgress(atIndexPath: indexPath)
+            tableView.reloadRows(at: [indexPath], with: .automatic)
+        }
+        deleteAction.backgroundColor = #colorLiteral(red: 0.9254902005, green: 0.2352941185, blue: 0.1019607857, alpha: 1)
+        addAction.backgroundColor = #colorLiteral(red: 0, green: 0.5898008943, blue: 1, alpha: 1)
+        
+        return [deleteAction,addAction]
+    }
+    
+    
+    
+    
 }
 
 extension GoalsVC {
@@ -84,10 +120,43 @@ extension GoalsVC {
     }
     
     
+    func removeGoal(atIndexPath indexPath:IndexPath){
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        managedContext.delete(goals[indexPath.row])
+        
+        
+        do {
+           try managedContext.save()
+            
+        }catch{
+            debugPrint("\(error.localizedDescription)")
+        }
+        
+    }
     
     
-    
-    
+    func setProgress(atIndexPath indexPath:IndexPath) {
+        guard let managedContext = appDelegate?.persistentContainer.viewContext else { return }
+        
+        let selectedRow = goals[indexPath.row]
+        
+        if selectedRow.goalProgress < selectedRow.goalCompletionValue {
+            selectedRow.goalProgress = selectedRow.goalProgress + 1
+        }else {
+            return
+        }
+        
+        do {
+            try managedContext.save()
+        }catch{
+            debugPrint("\(error)")
+        }
+        
+        
+        
+        
+        
+    }
     
     
     
--- SpecificGoals-app/View/GoalCell.swift
+++ SpecificGoals-app/View/GoalCell.swift
@@ -14,11 +14,16 @@ class GoalCell: UITableViewCell {
     @IBOutlet weak var goalTypeLbl: UILabel!
     @IBOutlet weak var progessLbl: UILabel!
     
+    @IBOutlet weak var completionView: UIView!
     func configureCell(goal:Goal){
         self.goalDescLbl.text = goal.goalDescription
         self.goalTypeLbl.text = goal.goalType
         self.progessLbl.text = String(describing: goal.goalProgress)
-
+        if goal.goalProgress == goal.goalCompletionValue {
+            completionView.isHidden = false
+        }else {
+            completionView.isHidden = true
+        }
     }
 
 }
